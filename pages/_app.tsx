b4e1f2fa555e42cb1e0f6765baf3e0290e330724
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2022-01-16 16:00:25 +0100

    ✨ add custom typing for Component with title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,15 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
-import Head from "next/head";
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }: AppPropsWithTitle) {
+  const title = Component.title || 'Default Title';
 
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>{title}</title>
       </Head>
       <Component {...pageProps} />
     </>
@@ -15,3 +17,8 @@ function MyApp({ Component, pageProps }: AppProps) {
 }
 
 export default MyApp;
+
+type AppPropsWithTitle = {
+  Component: AppProps['Component'] & { title?: string };
+  pageProps: AppProps['pageProps'];
+};
