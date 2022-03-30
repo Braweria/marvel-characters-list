06d5c0ba5e595dd31a3a38906a65d87f90996d26
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2022-03-30 19:37:06 +0200

    ✨ simple fetch wrapper

---
+++ utils/client.ts
@@ -0,0 +1,68 @@
+type Config = {
+  method: 'GET' | 'POST' | 'PATCH' | 'DELETE';
+  body?: string;
+  headers?: Record<string, string>;
+  [key: string]: unknown;
+};
+
+export async function client(
+  endpoint: string,
+  { body, method, ...customConfig }: Config
+) {
+  const headers = { 'Content-Type': 'application/json' };
+
+  let config: Config = {
+    method,
+    ...customConfig,
+    headers: {
+      ...headers,
+      ...customConfig.headers,
+    },
+  };
+
+  if (body) {
+    config.body = JSON.stringify(body);
+  }
+
+  let data;
+  try {
+    const response = await fetch(endpoint, config);
+    data = await response.json();
+    if (response.ok) {
+      return {
+        status: response.status,
+        data,
+        headers: response.headers,
+        url: response.url,
+      };
+    }
+    throw new Error(response.statusText);
+  } catch (error) {
+    console.log('__ERROR__', error);
+    throw new Error(error?.message ? error.message : data);
+  }
+}
+
+client.get = (endpoint: string, customConfig?: Omit<Config, 'method'>) => {
+  return client(endpoint, { ...customConfig, method: 'GET' });
+};
+
+client.post = (
+  endpoint: string,
+  body: string,
+  customConfig?: Omit<Config, 'method'>
+) => {
+  return client(endpoint, { body, ...customConfig, method: 'POST' });
+};
+
+client.patch = (
+  endpoint: string,
+  body: string,
+  customConfig?: Omit<Config, 'method'>
+) => {
+  return client(endpoint, { body, ...customConfig, method: 'PATCH' });
+};
+
+client.delete = (endpoint: string, customConfig?: Omit<Config, 'method'>) => {
+  return client(endpoint, { ...customConfig, method: 'DELETE' });
+};
