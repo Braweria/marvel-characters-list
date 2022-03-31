e3bc2be9996792476deb876a836cacfa6bce0a22
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2022-03-31 17:16:00 +0200

    🚧 virtuoso list

--- components/List/List.tsx
+++ components/List/List.tsx
@@ -1,16 +1,34 @@
 import { useSelector } from 'react-redux';
-import { selectAllCharacters } from 'redux/slices/characters.slice';
+import { Components, Virtuoso } from 'react-virtuoso';
+import {
+  selectAllCharacters,
+  selectCharacterStatus,
+} from 'redux/slices/characters.slice';
 
 export function List() {
   const characters = useSelector(selectAllCharacters);
+  const characterLoadingStatus = useSelector(selectCharacterStatus);
 
   console.log(characters);
+  console.log(characterLoadingStatus);
 
   return (
-    <ul>
-      {characters.map((character) => (
-        <li key={character.id}>{character.name}</li>
-      ))}
-    </ul>
+    characterLoadingStatus === 'idle' && (
+      <Virtuoso
+        style={{ height: '100%', width: '100%' }}
+        data={characters}
+        itemContent={(index, character) => <span>{character.name}</span>}
+        components={{
+          Item,
+        }}
+      />
+    )
   );
+}
+
+function Item({
+  children,
+  ...props
+}: Components['Item'] & { children: React.ReactNode }) {
+  return <li {...props}>{children}</li>;
 }
