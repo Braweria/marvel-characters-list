da13f8dbe703032641f011e2224fde548ade4928
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2022-04-01 19:32:53 +0200

    ✨ include comic slice in store

--- redux/store.ts
+++ redux/store.ts
@@ -1,13 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { createWrapper } from 'next-redux-wrapper';
+import type { TypedUseSelectorHook } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 import { characterReducer } from './slices/characters.slice';
+import { comicReducer } from './slices/comics.slice';
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export const makeStore = () =>
   configureStore({
     reducer: {
       characters: characterReducer,
+      comics: comicReducer,
     },
   });
 
@@ -16,3 +20,4 @@ export const wrapper = createWrapper(makeStore);
 export type AppStore = ReturnType<typeof makeStore>;
 export type AppDispatch = AppStore['dispatch'];
 export type RootState = ReturnType<AppStore['getState']>;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
