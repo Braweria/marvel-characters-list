fb2cc493819590900b7fb0e3777d38a8fc974449
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2022-04-01 19:32:09 +0200

    ✨ fetch character and return 404 if null

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -1,8 +1,15 @@
-import { selectCharacterById } from 'redux/slices/characters.slice';
-import { wrapper } from 'redux/store';
+import {
+  fetchCharacterById,
+  selectCharacterById,
+} from 'redux/slices/characters.slice';
+import { useAppSelector, wrapper } from 'redux/store';
 
 import { Avatar } from '@/Avatar/Avatar';
-import { useSelector } from 'react-redux';
+import { CharacterPageProps } from '~/types';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import { ResourceGrid } from '@/Grid/ResourceGrid';
+import { Character } from 'redux/typesSlice';
 
 /**
  * stories
@@ -11,14 +18,14 @@ import { useSelector } from 'react-redux';
  * series
  */
 
-export default function CharacterPage({ id }) {
-  const character = useSelector((state) => selectCharacterById(state, id));
-  console.log(character.name);
+export default function CharacterPage({ character }: { character: Character }) {
+  // console.log(character);
   return (
     <div>
       <h1>{character.name}</h1>
       {/* <Avatar src={character.image} alt={character.name} /> */}
       <p>{character.description}</p>
+      <ResourceGrid items={character.comics.items} />
     </div>
   );
 }
@@ -26,10 +33,21 @@ export default function CharacterPage({ id }) {
 export const getServerSideProps = wrapper.getServerSideProps(
   (store) =>
     async ({ params }) => {
-      const { id } = params;
+      if (!params || !params.id) {
+        return { notFound: true };
+      }
+
+      const character = await store.dispatch(
+        fetchCharacterById(Number(params.id))
+      );
+
+      if (!character.payload) {
+        return { notFound: true };
+      }
+
       return {
         props: {
-          id,
+          character: character.payload,
         },
       };
     }
