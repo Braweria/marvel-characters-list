fd1dbf3d39c25c2783b0d864cb173bb4c4c1317e
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2022-04-02 17:21:19 +0200

    ✨ add back link

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,8 +1,13 @@
+import Link from 'next/link';
+
 export default function FourOhFour() {
   return (
     <div>
       <h1>404</h1>
       <p>Page not found</p>
+      <Link href="/">
+        <a>Go Back</a>
+      </Link>
     </div>
   );
 }
